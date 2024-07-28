51f8f843ef63942fbe1542482011e9ec37721c1f
desarrollomalv <manuel.e.desarrollo@gmail.com>
2024-07-28 18:20:49 -0300

    Correccion render

--- back-end/app.py
+++ back-end/app.py
@@ -2,12 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+# Crear una instancia de la aplicación Flask
 app = Flask(__name__)
+
+# Configurar la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Desactivar la notificación de modificaciones de la base de datos
+
+# Inicializar SQLAlchemy
 db = SQLAlchemy(app)
+
+# Habilitar CORS
 CORS(app)
 
+# Importar las rutas después de inicializar la aplicación
 import routes
-
-if __name__ == '__main__':
-    app.run(debug=True)
