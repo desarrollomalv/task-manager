2fe5f1fa9708a24451821469a6b497b39b4c8711
desarrollomalv <manuel.e.desarrollo@gmail.com>
2024-07-28 19:43:56 -0300

    WSGI 5

--- back-end/wsgi.py
+++ back-end/wsgi.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Añadir el directorio 'back-end' al PYTHONPATH
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'back-end')))
-
 from app import app
 
 if __name__ == "__main__":
