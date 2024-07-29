385c337ec85ea3c061129bf7e6483ce8143fe569
desarrollomalv <manuel.e.desarrollo@gmail.com>
2024-07-29 20:59:31 -0300

    AGREGUE OBSERVACIONES 2

--- back-end/models.py
+++ back-end/models.py
@@ -1,5 +1,3 @@
-# models.py
-
 from app import db
 
 class Task(db.Model):
@@ -8,4 +6,6 @@ class Task(db.Model):
     responsable = db.Column(db.String(100), nullable=False)
     accion_recomendada = db.Column(db.String(200), nullable=False)
     estado_actual = db.Column(db.String(100), nullable=False)
+    prioridad = db.Column(db.String(50), nullable=False)  # Nueva columna para prioridad
+    observacion = db.Column(db.String(500), nullable=True)  # Nueva columna para observaciones
     archivo = db.Column(db.String(200), nullable=True)
