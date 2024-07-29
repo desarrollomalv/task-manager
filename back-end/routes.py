65eaba2d2602eb7b515ff34bd40ae95abeb540e7
desarrollomalv <manuel.e.desarrollo@gmail.com>
2024-07-29 20:27:14 -0300

    CAMBIOS EN APP, Y ROUTES PARA AGREGAR ARCHIVOS

--- back-end/routes.py
+++ back-end/routes.py
@@ -1,30 +1,30 @@
-import os
-from flask import request, jsonify
+from flask import request, jsonify, send_from_directory, send_file
 from app import app, db
 from models import Task
+import os
+
+UPLOAD_FOLDER = 'uploads'  # Define tu carpeta de subidas
 
 @app.route('/tasks', methods=['GET', 'POST'])
 def manage_tasks():
     if request.method == 'POST':
         data = request.form
-        file = request.files.get('archivo')
-
-        # Guardar el archivo si existe
-        filename = ''
-        if file:
-            filename = file.filename
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        archivo = request.files.get('archivo')
+        archivo_nombre = ''
+        if archivo:
+            archivo_nombre = archivo.filename
+            archivo.save(os.path.join(UPLOAD_FOLDER, archivo_nombre))
 
         new_task = Task(
-            tarea=data.get('tarea'),
-            responsable=data.get('responsable'),
-            accion_recomendada=data.get('accion_recomendada'),
-            estado_actual=data.get('estado_actual'),
-            archivo=filename
+            tarea=data['tarea'],
+            responsable=data['responsable'],
+            accion_recomendada=data['accion_recomendada'],
+            estado_actual=data['estado_actual'],
+            archivo=archivo_nombre
         )
         db.session.add(new_task)
         db.session.commit()
-        return jsonify({"id": new_task.id, "message": "Task created"}), 201
+        return jsonify({"message": "Task created", "id": new_task.id}), 201
 
     tasks = Task.query.all()
     return jsonify([{
@@ -41,8 +41,14 @@ def delete_task(id):
     task = Task.query.get(id)
     if task:
         if task.archivo:
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], task.archivo))
+            archivo_path = os.path.join(UPLOAD_FOLDER, task.archivo)
+            if os.path.exists(archivo_path):
+                os.remove(archivo_path)
         db.session.delete(task)
         db.session.commit()
         return jsonify({"message": "Task deleted"}), 200
     return jsonify({"message": "Task not found"}), 404
+
+@app.route('/uploads/<filename>', methods=['GET'])
+def download_file(filename):
+    return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
