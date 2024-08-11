a742265fbf5248913b6df05f441e5ae40a668097
desarrollomalv <manuel.e.desarrollo@gmail.com>
2024-08-11 14:16:19 -0300

    PRUEBA MIGRACION

--- back-end/models.py
+++ back-end/models.py
@@ -1,8 +1,9 @@
-# models.py
-
 from app import db
 
 class Task(db.Model):
+    __tablename__ = 'task'
+    __table_args__ = {'schema': 'public'}  # Asegura que el esquema sea 'public'
+
     id = db.Column(db.Integer, primary_key=True)
     tarea = db.Column(db.String(100), nullable=False)
     responsable = db.Column(db.String(100), nullable=False)
@@ -10,4 +11,4 @@ class Task(db.Model):
     estado_actual = db.Column(db.String(50), nullable=False)
     prioridad = db.Column(db.String(20), nullable=True)
     archivo = db.Column(db.String(100), nullable=True)
-    observaciones = db.Column(db.Text, nullable=True)  # Nuevo campo
+    observaciones = db.Column(db.Text, nullable=True)
