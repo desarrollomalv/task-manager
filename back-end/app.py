c0461e9ac44822c2f258e9770be9a27ab75a05e9
desarrollomalv <manuel.e.desarrollo@gmail.com>
2024-08-11 00:17:47 -0300

    BASE DE DATOS NUEVA

--- back-end/app.py
+++ back-end/app.py
@@ -8,7 +8,7 @@ from flask_migrate import Migrate  # Importa Migrate
 app = Flask(__name__)
 
 # Configurar la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tareas:lRqaw5Bb6aXW0zB3dLOACiY24ulp6NSW@dpg-cqlc4f3qf0us73996msg-a.oregon-postgres.render.com/tareas_6ic6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.npmeznwsdenruazxcptw:F42n20BTu04F3dOV@aws-0-us-west-1.pooler.supabase.com:6543/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'uploads'  # Carpeta para archivos subidos
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limitar el tamaño del archivo a 16MB
